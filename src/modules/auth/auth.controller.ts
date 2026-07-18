5c529547de6e18c9d49a5472c1c5a0ae64443420
MRM Abdullah <mrmabdullah2024@gmail.com>
2026-07-18 00:42:42 -0400

    user login successfully

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { authService } from "./auth.service";
+
+const loginUser = catchAsync(async (req : Request, res : Response, next: NextFunction ) => {
+    const payload = req.body;
+    const loginResult = await authService.loginUser(payload);
+    sendResponse(res, {
+        statusCode : 200,
+        success : true,
+        message : "User logged in successfully",
+        data : loginResult
+    })
+
+})
+
+export const authController = {
+    loginUser
+}
